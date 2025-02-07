dc57da6bf1aab6b227967bdd76037956e5c4d767
horat1us <reclamme+github@gmail.com>
2025-02-07 13:43:30 +0100

    Remove URI from ConfigInterface (to be moved to Service)

--- src/ConfigInterface.php
+++ src/ConfigInterface.php
@@ -6,7 +6,6 @@ namespace Wearesho\Delivery\TurboSms;
 
 interface ConfigInterface
 {
-    public const URI = 'https://api.turbosms.ua/';
     public const SENDER = 'Msg';
 
     public function getHttpToken(): string;
